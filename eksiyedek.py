d8ad2fc5a89658c35233cd2ea4fc8342af5cf39f
Çağrı Özkurt <87033144+cagriozkurt@users.noreply.github.com>
2021-10-29 09:50:27 +0300

    Update eksiyedek.py
    2000 öncesi bazı girdilerde saat bilgisi bulunmadığı için ortaya çıkan hata düzeltildi.

--- eksiyedek.py
+++ eksiyedek.py
@@ -35,11 +35,17 @@ except AttributeError:
     print("Yazar kayıp.")
 else:
     entries_count = re.findall(r"\d+", entries_count_text)[0]
+    if int(entries_count[-1]) < 5:
+        number_of_pages = round(int(entries_count) / 10) + 1
+    else:
+        number_of_pages = round(int(entries_count) / 10)
+    print(str(number_of_pages) + " sayfa entry indirilecek...")
     root = ET.Element("backup", attrib={"nick": nick, "backupdate": backupdate})
     m1 = ET.Element("entries", attrib={"count": entries_count})
     root.append(m1)
     n = 1
     while True:
+        print(f"{n} / {number_of_pages}")
         page_url = url + f"&p={n}"
         page_r = requests.get(page_url, headers=headers)
         page_r.raise_for_status()
@@ -51,37 +57,48 @@ else:
             for entry in entries:
                 entry_id = entry.find("li")["data-id"]
                 entry_title = entry.find("h1")["data-title"]
-                print(entry_title)
                 entry_date = entry.find(
                     "a", {"class": "entry-date permalink"}
                 ).get_text()
-                date_regex = re.findall(
-                    r"(\d{2})\.(\d{2})\.(\d{4}) (\d{2}:\d{2})", entry_date
-                )[0]
-                new_date_format = (
-                    date_regex[2]
-                    + "-"
-                    + date_regex[1]
-                    + "-"
-                    + date_regex[0]
-                    + "T"
-                    + date_regex[3]
-                    + ":00"
-                )
-                entry_content = entry.find("div", {"class": "content"})
-                for br in entry_content.findAll("br"):
-                    br.replace_with("\n")
-                b = ET.SubElement(
-                    m1,
-                    "entry",
-                    attrib={
-                        "title": entry_title,
-                        "id": entry_id,
-                        "date": new_date_format,
-                    },
-                )
-                b.text = entry_content.get_text()
-            n += 1
+                try:
+                    date_regex = re.findall(
+                        r"(\d{2})\.(\d{2})\.(\d{4}) (\d{2}:\d{2})", entry_date
+                    )[0]
+                    new_date_format = (
+                        date_regex[2]
+                        + "-"
+                        + date_regex[1]
+                        + "-"
+                        + date_regex[0]
+                        + "T"
+                        + date_regex[3]
+                        + ":00"
+                    )
+                except IndexError:
+                    date_regex = re.findall(r"(\d{2})\.(\d{2})\.(\d{4})", entry_date)[0]
+                    new_date_format = (
+                        date_regex[2]
+                        + "-"
+                        + date_regex[1]
+                        + "-"
+                        + date_regex[0]
+                        + "T00:00:00"
+                    )
+                else:
+                    entry_content = entry.find("div", {"class": "content"})
+                    for br in entry_content.findAll("br"):
+                        br.replace_with("\n")
+                    b = ET.SubElement(
+                        m1,
+                        "entry",
+                        attrib={
+                            "title": entry_title,
+                            "id": entry_id,
+                            "date": new_date_format,
+                        },
+                    )
+                    b.text = entry_content.get_text()
+        n += 1
 
     tree = ET.ElementTree(root)
     with open(fileName, "wb") as f:
